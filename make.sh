dd61029a0e371bf66db4f32d98d5dcde8118d690
Bambo-Borris <alanmwalker96@gmail.com>
2025-01-01 22:16:24 +0000

    Setup make script for linux

--- make.sh
+++ make.sh
@@ -29,48 +29,18 @@ releaseFlags=(
     "-no-type-assert"
 )
 
-# Build logic based on arguments
-if [ "$arg0" == "nce" ]; then
-    if [ "$arg1" == "release" ]; then
-        odin build "src/nce" "${commonFlags[@]}" "${releaseFlags[@]}" -out:"build/nce.a" -build-mode:shared 
-    else
-        odin build "src/nce" "${commonFlags[@]}" "${debugFlags[@]}" -out:"build/nce.a" -build-mode:shared
-    fi
-elif [ "$arg0" == "redshift" ]; then
-    if [ "$arg1" == "release" ]; then
-        odin build "src/redshift" "${commonFlags[@]}" "${releaseFlags[@]}" -out:"build/redshift-assault" -build-mode:exe
-    else
-        odin build "src/redshift" "${commonFlags[@]}" "${debugFlags[@]}" -out:"build/redshift-assault" -build-mode:exe -debug 
-    fi
-    
-    if [ $? -eq 1 ]; then
-        echo "Build command failed!"
-        exit 1
-    fi
+if [ "$arg1" == "release" ]; then
+    odin build "src/" "${commonFlags[@]}" "${releaseFlags[@]}" -out:"build/vulkan" -build-mode:exe
+else
+    odin build "src/" "${commonFlags[@]}" "${debugFlags[@]}" -out:"build/vulkan" -build-mode:exe -debug 
+fi
 
-    # If 'run' is specified as the second or third argument, run the executable
-    if [ "$arg2" == "run" ] || [ "$arg1" == "run" ]; then
-        ./build/redshift-assault
-    elif [ "$arg2" == "runmove" ] || [ "$arg1" == "runmove" ]; then
-        ./build/redshift-assault MOVEMENT
-    fi
-elif [ "$arg0" == "ohw" ]; then
-    if [ "$arg1" == "release" ]; then
-        odin build "src/ohw" "${commonFlags[@]}" "${releaseFlags[@]}" -out:"build/ohw" -build-mode:exe
-    else
-        odin build "src/ohw" "${commonFlags[@]}" "${debugFlags[@]}" -out:"build/ohw" -build-mode:exe -debug 
-    fi
-    
-    if [ $? -eq 1 ]; then
-        echo "Build command failed!"
-        exit 1
-    fi
+if [ $? -eq 1 ]; then
+    echo "Build command failed!"
+    exit 1
+fi
 
-    # If 'run' is specified as the second or third argument, run the executable
-    if [ "$arg2" == "run" ] || [ "$arg1" == "run" ]; then
-        ./build/ohw
-    fi
-    # elif [ "$arg2" == "runmove" ] || [ "$arg1" == "runmove" ]; then
-    #     ./build/redshift-assault MOVEMENT
-    # fi
+# If 'run' is specified as the second or third argument, run the executable
+if [ "$arg1" == "run" ] || [ "$arg0" == "run" ]; then
+    ./build/vulkan
 fi
